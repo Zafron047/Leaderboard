a57d1a62e735ed251ad3593775341176784df07f
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-06-21 23:27:38 +0600

    Add Clear DOM function in render.js

--- src/Modules/Render.js
+++ src/Modules/Render.js
@@ -2,6 +2,8 @@ import { array } from '..';
 import Dom from './Dom';
 
 const render = () => {
+  const listBox = document.querySelector('#list-box');
+  listBox.textContent = '';
   array.forEach((Obj) => {
     Dom(Obj.Name, Obj.Score);
   });
