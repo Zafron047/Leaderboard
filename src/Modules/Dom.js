01318beb89f7c10d38a6ff4aa330b48097223b6b
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-06-24 23:39:41 +0600

    Add final touch

--- src/Modules/Dom.js
+++ src/Modules/Dom.js
@@ -5,6 +5,12 @@ const dom = (user, score) => {
   listItem.id = 'list-item';
   listItem.className = 'list-item';
 
+  if (listBox.childElementCount % 2 === 1) {
+    listItem.style.backgroundColor = 'skyblue';
+  } else {
+    listItem.style.backgroundColor = 'rgba(173, 216, 230, 0.404)';
+  }
+  
   listBox.appendChild(listItem);
 };
 
